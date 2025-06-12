9ac8ca04bfcfe3285fc9a1481eed954d35ecc25c
Rohit Sharma <rsantoshsharma9730@gmail.com>
2025-06-12 16:38:31 +0530

    Notification import issue fix

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { deleteUserNotifications, getUserNotifications } from "../controllers/notificationController.js";
-import { authmiddel } from "../middleware/authMiddleware.js";
+import {  authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("./notification", authmiddel, getUserNotifications);
-router.delete("./deleteAll",  authmiddel, deleteUserNotifications);
+router.get("./notification",  authenticate, getUserNotifications);
+router.delete("./deleteAll",   authenticate, deleteUserNotifications);
 
 export default router;
