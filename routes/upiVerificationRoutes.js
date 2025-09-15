4ddd49b21df82bebc1bfb24da14e56d549913e91
Raman Chauhan <ramanchauhan13@gmail.com>
2025-09-16 00:54:03 +0530

    Add direct UPI verification function and update routes

--- routes/upiVerificationRoutes.js
+++ routes/upiVerificationRoutes.js
@@ -1,15 +1,13 @@
 // routes/upiVerificationRoutes.js
 import express from "express";
-// import { requestUpiVerification, verifyUpiOtp } from "../controllers/upiVerificationController.js";
+import { requestUpiVerification, directUpiVerification, verifyUpiOtp } from "../controllers/upiVerificationController.js";
 import { authenticate } from "../middleware/authMiddleware.js";
 import { checkUserStatus } from "../middleware/checkuserstatus.js";
 
 const router = express.Router();
 
 // router.post("/verify-upid", authenticate, checkUserStatus, requestUpiVerification);
-// router.post("/verify-upid/otp", authenticate, checkUserStatus, verifyUpiOtp);
-
-router.post("/verify-upid", authenticate, checkUserStatus);
-router.post("/verify-upid/otp", authenticate, checkUserStatus);
+router.post("/verify-upid/otp", authenticate, checkUserStatus, verifyUpiOtp);
+router.post("/verify-upid", authenticate, checkUserStatus, directUpiVerification);
 
 export default router;
