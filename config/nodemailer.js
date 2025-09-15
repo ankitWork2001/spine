e4431b301f9a71a9def519d21c2acd7e5820aa45
Raman Chauhan <ramanchauhan13@gmail.com>
2025-09-15 22:07:43 +0530

    Refactor email transport configuration to use service property for Gmail

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,10 +1,7 @@
 import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
-  // service: "gmail",
-   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth:{
 user: process.env.EMAIL_USER,
 pass: process.env.EMAIL_PASS
--- utils/sendMail.js
+++ utils/sendMail.js
@@ -8,9 +8,7 @@ const sendEmail = async (to, subject, otp) => {
 
   try {
     const transporter = nodemailer.createTransport({
-       host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465, false for 587
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
