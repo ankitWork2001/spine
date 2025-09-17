b44b3ec5afaefb7e2ff19b63239a27ea7a0d57b4
Raman Chauhan <ramanchauhan13@gmail.com>
2025-09-17 21:40:44 +0530

    for future enchancement

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -3,7 +3,7 @@ import Notification from '../models/notificationModel.js';
 export const getUserNotifications = async (req, res) => {
     try {
         const userId = req.userId; // Extract from middleware or token
-        const notifications = await Notification.find({ userId });
+        const notifications = await Notification.find({ userId }).sort({ createdAt: -1 });
         res.status(200).json({ success: true, notifications });
     } catch (error) {
         res.status(500).json({ success: false, error: error.message });
